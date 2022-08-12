f51ef0f271a8beaa79a9cb4b00afc2766c88578d
kirill24097 <110169552+kirill24097@users.noreply.github.com>
2022-08-12 07:45:55 +0300

    HW metod1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+/// 1
+        printLeapYear(2022);
+
+        /// 2
+        int clientOS = 1;
+        int deviseYear = 2021;
+        printSuggestionForInstall(clientOS, deviseYear);
+
+        ///3
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        int interval = 40;
+        int startInterval = 20;
+        if (deliveryDistance <= startInterval) {
+            deliveryDays = 1;
+        } else {
+            deliveryDays = deliveryDays + (int) Math.ceil(deliveryDistance - startInterval / (double) interval);
+        }
+        System.out.println("требуется дней" + deliveryDays);
+    }
+
+
+    private static void printSuggestionForInstall(int client, int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        String output;
+        if (deviceYear < currentYear) {
+            output = "установи облегченную версию";
+        } else {
+            output = "установи  версию";
+        }
+        int clientOS = 1;
+        if (clientOS == 1) {
+            output += "андроид по ссылке";
+        } else {
+            output += "IOS по ссылке";
+        }
+        System.out.println(output);
+
+    }
+
+
+    private static void printLeapYear(int year) {
+        String output;
+        if (isLeapYear(year)) {
+            output = (year + "год високосный");
+        } else {
+            output = (year + "год не високосный");
+        }
+        System.out.println(output);
+    }
+
+    private static boolean isLeapYear(int year) {
+
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
     }
 }
